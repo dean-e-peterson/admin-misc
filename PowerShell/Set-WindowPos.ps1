d261fac2d07782cbc01f719f873f9f1932de13d2
Dean Peterson <dean.e.penguin@gmail.com>
2026-08-02 16:39:35 -0400

    Put C# win resizing source in PowerShell script, adjust for .NET Framework 4.8, and get working.

--- PowerShell/Set-WindowPos.ps1
+++ PowerShell/Set-WindowPos.ps1
@@ -1,19 +1,53 @@
-# $Process = Get-Process -Name explorer
-# Write-Host $Process
-# Write-Host $Process.Handle
+param(
+    [Parameter(Mandatory=$True)]
+    [string]$folder
+)
 
+$Source = @"
+using System;
+using System.Runtime.InteropServices;
+namespace Native
+{
+    public static class User32
+    {
+        // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowpos
+        private static readonly IntPtr HWND_BOTTOM = (IntPtr)(1);
+        private static readonly IntPtr HWND_NOTOPMOST = (IntPtr)(-2);
+        private static readonly IntPtr HWND_TOP = (IntPtr)(0);
+        private static readonly IntPtr HWND_TOPMOST = (IntPtr)(-1);
+        private const uint SWP_NOZORDER = 0x0004;
+        private const uint SWP_NOACTIVATE = 0x0010;
+        private const uint SWP_NOOWNERZORDER = 0x0200;
+        private const uint SWP_ASYNCWINDOWPOS = 0x4000;
 
-# $shell = New-Object -ComObject Shell.Application
-# # $shell | Get-Member
-#
-# $windowList = $shell.Windows()
-# Write-Output $windowList.Count
-# foreach ($window in $windowList)
-# {
-#     Write-Output "In loop"
-#     # $window | Get-Member
-#     Write-Output $window.Name
-# }
+        [DllImport("User32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(
+            IntPtr hWnd,
+            IntPtr hWndInsertAfter,
+            int  X,
+            int  Y,
+            int  cx,
+            int  cy,
+            uint uFlags
+        );
+
+        public static void SetWindowPos(IntPtr hWnd, int X, int Y, int cx, int cy)
+        {
+            // Ignored if SWP_NOZORDER is in uFlags.
+            IntPtr hWndInsertAfter = HWND_TOP;
+            uint uFlags = SWP_NOZORDER | SWP_NOACTIVATE;
+            if (!SetWindowPos(hWnd, hWndInsertAfter, X, Y, cx, cy, uFlags))
+            {
+                int lastError = Marshal.GetLastWin32Error();
+                throw new Exception("Error " + lastError + " calling SetWindowPos().");
+            }
+        }
+    }
+}
+"@
+
+Add-Type -TypeDefinition $Source
 
 # Borrowed from https://github.com/microsoft/PowerToys/blob/d2c53bf3861ed2688a1c30aafd66ea0fc0186399/.github/skills/powertoys-verification/scripts/pt-explorer-com.ps1#L14
 function Get-PtExplorerWindows {
@@ -28,5 +62,16 @@ function Get-PtExplorerWindows {
     } catch { return @() }
 }
 
+$localUri = (New-Object System.Uri $folder).AbsoluteUri
+# Write-Host $localUri
+
 $windows = Get-PtExplorerWindows
-Write-Output $windows | Format-List
+# Write-Output $windows.Count
+foreach ($window in $windows) {
+    # Write-Output "-----------------"
+    # Write-Output $window | Format-List
+    if ($window.LocationURL -eq $localUri)
+    {
+        [Native.User32]::SetWindowPos($window.HWND, 30, 14, 1000, 500)
+    }
+}
