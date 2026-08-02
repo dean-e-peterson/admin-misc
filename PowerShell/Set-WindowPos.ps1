a560954937b4c07d5c78529f7221553eb965176e
Dean Peterson <dean.e.penguin@gmail.com>
2026-08-02 11:28:32 -0400

    A shell window, my kingdom for a shell window.

--- PowerShell/Set-WindowPos.ps1
+++ PowerShell/Set-WindowPos.ps1
@@ -1,3 +1,17 @@
-$Process = Get-Process -Name explorer
-Write-Host $Process
-Write-Host $Process.Handle
+# $Process = Get-Process -Name explorer
+# Write-Host $Process
+# Write-Host $Process.Handle
+
+$shell = New-Object -ComObject Shell.Application
+# $shell | Get-Member
+
+$windowList = $shell.Windows()
+Write-Output $windowList
+foreach ($window in $windowList)
+{
+    Write-Output "In loop"
+    $window | Get-Member
+    Write-Output $window.Name
+    break
+}
+
